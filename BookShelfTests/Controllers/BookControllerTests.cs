d71d50760e5eb21ec415906cbf37c984cb9d3c9e
kirknaoborot <drishik05@gmail.com>
2021-05-10 13:51:51 +0300

    Добавил тест
    Попробовал написать тест индекс метода

--- BookShelfTests/Controllers/BookControllerTests.cs
+++ BookShelfTests/Controllers/BookControllerTests.cs
@@ -4,43 +4,37 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Moq;
-using BookShelf.Interfaces;
 using BookShelf.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BookShelf.Controllers.Tests
 {
     [TestClass()]
     public class BookControllerTests
     {
-        [TestMethod()]
-        public void BookControllerTest()
-        {
-            Assert.Fail();
-        }
 
-        [TestMethod()]
+
+        [Fact]
         public void IndexTest()
         {
+            var options = new DbContextOptionsBuilder<BookContext>()
+           .UseInMemoryDatabase(databaseName: "test")
+           .Options;
 
+            // Use a clean instance of the context to run the test
+            using (var context = new BookContext(options))
+            {
+                var controller = new BookController(null, context);
+                List<Book> books = context.Books.ToList();
+
+                Xunit.Assert.Equal(4, books.Count);
+
+            }
         }
-
-        [TestMethod()]
-        public void CreateBookTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void CreateBookTest1()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void DeleteBookTest()
-        {
-            Assert.Fail();
-        }
-
     }
 }
+
+    
